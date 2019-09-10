029f62b54f8fbcde2a7e6046ccdb65c71e100e15
alex <chulgarev@gmail.com>
2019-09-10 10:59:42 +0700

    MotionVectorsMap done

--- video_compensation/videocompensation.h
+++ video_compensation/videocompensation.h
@@ -1,15 +1,17 @@
 #ifndef VIDEOCOMPENSATION_H
 #define VIDEOCOMPENSATION_H
 
-#include "motionvector.h"
-
 #include <frame.h>
+#include <map>
+
+using MotionVector     = std::map<std::string,int>;
+using MotionVectorsMap = std::map<int, std::map<int, MotionVector >>;
 
 class VideoCompensation
 {
 public:
     VideoCompensation(int searchAreaInBlocks = 1);
-    void findMotionVector(const Frame &currentFrame, const Frame &previousFrame);
+    MotionVectorsMap findMotionVectors(const Frame &currentFrame, const Frame &previousFrame);
 private:
     int SAD(const Block &block1, const Block &block2) const;
     MotionVector findVector(const Block &block, const Frame &frame);
