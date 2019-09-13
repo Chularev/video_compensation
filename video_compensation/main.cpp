b3691498d7e4ec2d58ce8cd2b5608cfc39237c20
alex <chulgarev@gmail.com>
2019-09-13 18:30:22 +0700

    main refactoring

--- video_compensation/main.cpp
+++ video_compensation/main.cpp
@@ -7,17 +7,28 @@
 
 using namespace std;
 
+void testParams(const std::map<std::string, std::string> &params)
+{
+    if (params.find("-w") == params.end())
+        throw std::invalid_argument("Param -w (frame width) is not existing");
+
+
+    if (params.find("-h") == params.end())
+        throw std::invalid_argument("Param -h (frame height) is not existing");
+
+    if (params.find("-pathInput") == params.end())
+        throw std::invalid_argument("Param -pathInput is not existing");
+
+    if (params.find("-pathOutput") == params.end())
+        throw std::invalid_argument("Param -pathOutput is not existing");
+
+}
+
 int main(int argc, char *argv[])
 {
     // -pathInput D://Qt_Projekts//video_compensation//tmpRamshta.yuv
     //-w 640 -h 360
     //-pathOutput D://Qt_Projekts//video_compensation//progResult.yuv
-
-
-    std::string path("D://Qt_Projekts//video_compensation//");
-    //std::string path("/home/alex/video_compensation/");
-    //640x360
-
 
     try
     {
@@ -30,43 +41,36 @@ int main(int argc, char *argv[])
         for (int count = 1; count < argc; count += 2)
             params[argv[count]] = argv[count + 1];
 
-        if (params.find("-w") == params.end())
-            throw std::invalid_argument("Param -w (frame width) is not existing");
-
-
-        if (params.find("-h") == params.end())
-            throw std::invalid_argument("Param -h (frame height) is not existing");
-
+        testParams(params);
 
         int width  = std::stoi(params["-w"]);
         int height = std::stoi(params["-h"]);
         FrameInfo::init(width, height);
 
-        if (params.find("-pathInput") == params.end())
-            throw std::invalid_argument("Param -pathInput is not existing");
-
         FileReader reader(params["-pathInput"]);
         reader.open();
 
-
-        if (params.find("-pathOutput") == params.end())
-            throw std::invalid_argument("Param -pathOutput is not existing");
 
         FileWriter writer(params["-pathOutput"]);
         writer.open();
 
 
         VideoCompensation handler(1);
-        for (int count = 0; count < 151; count++)
-        {
-            Frame previousFrame = reader.readeFrame(count);
-            Frame currentFrame = reader.readeFrame(count + 1);
+
+        Frame currentFrame(0);
+        if (!reader.eof())
+            currentFrame = reader.readeNext();
+
+        while ( !reader.eof()) {
+            Frame previousFrame = currentFrame;
+            currentFrame = reader.readeNext();
+
             MotionVectorsMap vectorMap = handler.findMotionVectors(currentFrame,previousFrame);
             Frame result = handler.doCompensation(currentFrame,previousFrame,vectorMap);
             writer.writeFrame(result);
-
-            cout << "   Frame number = " << result.index();
+            //cout << "   Frame number = " << result.index();
         }
+
 
         reader.close();
         writer.close();
