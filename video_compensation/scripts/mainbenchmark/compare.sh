ec5314a1d19bf1805de858681de6d39711321063
Alex <chulgarev@gmail.com>
2025-07-16 22:20:45 +0700

    Added all scripts

--- video_compensation/scripts/mainbenchmark/compare.sh
+++ video_compensation/scripts/mainbenchmark/compare.sh
@@ -4,4 +4,4 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd "$SCRIPT_DIR" || exit
 
-cd .. && compare.py benchmarks mainbenchmark.json tmp_mainbenchmark.json 
+compare.py benchmarks mainbenchmark.json tmp_mainbenchmark.json 
